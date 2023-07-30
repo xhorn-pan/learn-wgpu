590f63ad55de24e4a5c3236c62b636e1f3f6f67f
xhorn-pan <23328176+xhorn-pan@users.noreply.github.com>
2023-07-30 10:55:10 -0400

    buffer.start.1

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,21 @@ struct Vertex {
     color: [f32; 3],
 }
 
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
+
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
